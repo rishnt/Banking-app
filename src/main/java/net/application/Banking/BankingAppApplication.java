f4d148c073cb86035e4d46eab7662057190fab4e
rishnt <94091947+rishnt@users.noreply.github.com>
2025-04-22 21:28:13 +0530

    initial commit

---
+++ src/main/java/net/application/Banking/BankingAppApplication.java
@@ -0,0 +1,13 @@
+package net.application.Banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppApplication.class, args);
+	}
+
+}
